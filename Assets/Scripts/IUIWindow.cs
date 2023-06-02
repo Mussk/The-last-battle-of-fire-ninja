0a17a76e453c3da449048070705d364c05d2d938
Mussk <sanja.vin1999@gmail.com>
2023-06-02 18:58:38 +0200

    initial commit

---
+++ Assets/Scripts/IUIWindow.cs
@@ -0,0 +1,10 @@
+using Krivodeling.UI.Effects;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IUIWindow 
+{
+    GameObject UIWindowObject { get; }
+    UIBlur UIBlur { get; }
+}
