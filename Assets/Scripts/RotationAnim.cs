95de813c8ab6cc06102e57b6c6b9db73d1b3e5c7
Mussk <sanja.vin1999@gmail.com>
2023-10-08 19:18:41 +0200

    rotation of skin preview + rotation reset

--- Assets/Scripts/RotationAnim.cs
+++ Assets/Scripts/RotationAnim.cs
@@ -1,22 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class RotationAnim : MonoBehaviour
+public class RotationAnim : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
 
     [SerializeField]
     private float rotationSpeed;
 
+    private bool mouseOver = false;
+
+    [SerializeField]
+    private GameObject playerSkinPreview;
+
+    private Quaternion startingRotation;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        startingRotation = playerSkinPreview.transform.rotation;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
+        if (mouseOver)
+        {
+            playerSkinPreview.transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("Rotation works");
+        mouseOver = true;
+       
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+       mouseOver = false;
+       playerSkinPreview.transform.rotation = startingRotation;
     }
 }
