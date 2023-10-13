ed458119cd06ffe13a286acb126b1db36c5f9adb
Mussk <sanja.vin1999@gmail.com>
2023-10-13 18:48:17 +0200

    moved health code to Character

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -2,10 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Character : MonoBehaviour
+public class Character : MonoBehaviour, IHasHealth
 {
     [SerializeField]
     public bool isMoving = true;
+
+    [SerializeField]
+    public SFXSoundManager SFXManager;
+
+    protected HealthSystem healthSystem;
+
+    [SerializeField]
+    protected int _currentHealth;
+
+    [SerializeField]
+    protected int _currentMaxHealth;
+
+    public int CurrentHealth => _currentHealth;
+   
+    public int CurrentMaxHealth => _currentMaxHealth;
+
+    public HealthSystem HealthSystem { get => healthSystem; set => healthSystem = value; }
 
     void Start()
     {
