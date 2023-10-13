da086154694c4d83ce655938c5932ffd5d8e4526
Mussk <sanja.vin1999@gmail.com>
2023-10-13 18:50:13 +0200

    added sound code

--- Assets/Scripts/MainMenuUI.cs
+++ Assets/Scripts/MainMenuUI.cs
@@ -18,16 +18,21 @@ public class MainMenuUI : MonoBehaviour
     [SerializeField]
     private Button quitButton;
 
+    [SerializeField]
+    private SoundPlayer soundPlayer;
+
     private SceneManipulationButtons sceneManipulationButtons;
 
     // Start is called before the first frame update
     void Start()
     {
-        sceneManipulationButtons = new SceneManipulationButtons();
+        sceneManipulationButtons = new SceneManipulationButtons(soundPlayer);
 
         playButton.onClick.AddListener(delegate { sceneManipulationButtons.LoadNewSceneOnClick(0); });
         shopButton.onClick.AddListener (delegate { sceneManipulationButtons.LoadNewSceneOnClick(1); });
         quitButton.onClick.AddListener(sceneManipulationButtons.QuitApplication);
+
+        
     }
 
     // Update is called once per frame
@@ -36,4 +41,6 @@ public class MainMenuUI : MonoBehaviour
         
     }
     
+    
+
 }
