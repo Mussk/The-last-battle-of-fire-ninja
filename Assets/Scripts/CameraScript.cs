acef7b5546fc12fb1e7a2b78a736143a4e1d2526
Mussk <sanja.vin1999@gmail.com>
2024-06-17 14:36:01 +0200

    camera statter fix

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -13,8 +13,7 @@ public class CameraScript : MonoBehaviour
         
     }
 
-    // Update is called once per frame
-    void FixedUpdate()
+    private void Update()
     {
         WatchPlayer();
     }
